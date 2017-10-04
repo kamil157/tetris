f80ef025abd7b8d17dfbcc7f3150be9a1af9bbf9
Kamil Chłodnicki <Kamil.Chlodnicki@tivo.com>
2017-10-04 10:08:54 +0200

    Use windows in UI

--- ui.py
+++ ui.py
@@ -6,37 +6,43 @@ from copy import deepcopy
 
 from tetris import Game
 
+block_width = 2
 
-def render_tetromino(stdscr, tetromino, block_width):
+
+def render_tetromino(stdscr, tetromino):
     for y, x in tetromino:
         for i in range(block_width):
-            stdscr.addstr(y, 2 * x + i, ' ', curses.color_pair(tetromino.color + 1))
+            stdscr.insch(y, 2 * x + i, ' ', curses.color_pair(tetromino.color + 1))
 
 
-def render(game, stdscr, fps_counter, key):
-    stdscr.clear()
-    block_width = 2
-    grid = game.get_grid()
+def render_game(game, game_win):
+    game_win.clear()
 
     # draw grid
-    for y, row in enumerate(grid):
+    for y, row in enumerate(game.get_grid()):
         for x, color in enumerate(row):
             for i in range(block_width):
-                stdscr.addstr(y, 2 * x + i, ' ', curses.color_pair(color + 1))
+                game_win.insch(y, 2 * x + i, ' ', curses.color_pair(color + 1))
 
-    render_tetromino(stdscr, game.active_tetromino, block_width)
+    render_tetromino(game_win, game.active_tetromino)
 
-    stdscr.addstr(0, 20, "Fps: {}".format(fps_counter))
-    stdscr.addstr(1, 20, "Key: {}".format(key))
-    stdscr.addstr(2, 20, "Score: {}".format(game.get_score()))
-    stdscr.addstr(3, 20, "Next:")
+    game_win.refresh()
+
+
+def render_info(game, fps_counter, key, info_win):
+    info_win.clear()
+
+    info_win.addstr(0, 0, "Fps: {}".format(fps_counter))
+    info_win.addstr(1, 0, "Key: {}".format(key))
+    info_win.addstr(2, 0, "Score: {}".format(game.get_score()))
+    info_win.addstr(3, 0, "Next:")
 
     next_tetromino = deepcopy(game.next_tetromino)
     next_tetromino.position_y = 4
-    next_tetromino.position_x = 10
-    render_tetromino(stdscr, next_tetromino, block_width)
+    next_tetromino.position_x = 0
+    render_tetromino(info_win, next_tetromino)
 
-    stdscr.refresh()
+    info_win.refresh()
 
 
 def main(stdscr):
@@ -57,6 +63,7 @@ def main(stdscr):
         curses.init_pair(6, curses.COLOR_WHITE, curses.COLOR_BLACK)  # L
 
     curses.curs_set(False)
+    stdscr.nodelay(True)
     game = Game()
 
     desired_fps = 60
@@ -64,12 +71,14 @@ def main(stdscr):
     frames = 0
     fps_counter = 0
 
+    game_win = curses.newwin(game.rows, 2 * game.cols, 0, 0)
+    info_win = curses.newwin(game.rows, 10, 0, 2 * game.cols)
+
     while True:
         frame_start = time()
 
         # Get user input
         key = None
-        stdscr.nodelay(True)
         try:
             key = stdscr.getkey()
         except curses.error:
@@ -83,7 +92,9 @@ def main(stdscr):
             start_fps = time()
 
         game.tick(key)
-        render(game, stdscr, fps_counter, key)
+
+        render_game(game, game_win)
+        render_info(game, fps_counter, key, info_win)
 
         sleep_time = frame_start + 1 / desired_fps - time()
         if sleep_time > 0:
